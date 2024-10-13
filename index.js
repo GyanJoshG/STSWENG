2d3b4a404961e95dca43ec7a24fb8a2fd7a06d2e
brent-soan <147591053+brent-soan@users.noreply.github.com>
2024-10-13 12:37:44 +0800

    Added .gitignore, package.json, views folder

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.use(session({
 //app.set('view engine', '.hbs');
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
