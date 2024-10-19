a8206ccc8f88afc2fde4c7f130216fcba4731fbe
brent-soan <147591053+brent-soan@users.noreply.github.com>
2024-10-19 19:29:07 +0800

    Created and applied controllers

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,8 @@
-// TODO: Implement controller to routes
 import express from 'express';
+import controller from './../../controllers/controller.js';
 const router = express.Router();
 
 // Go to home page
-router.get('/', (req, res) => {
-    try {
-        console.log('Requested home page');
-        res.status(200).render('index');
-    } catch (err) {
-        console.error(err);
-    }
-});
+router.get('/', controller.getIndex);
 
 export default router;
