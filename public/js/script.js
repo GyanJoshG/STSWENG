b3ca696385c8be69db76df0b46313b1114207b26
brent-soan <147591053+brent-soan@users.noreply.github.com>
2024-10-21 10:27:00 +0800

    Added products stock

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let cart = [];
-    let currentSlide = 0;
+document.addEventListener('DOMContentLoaded', async () => {
+    // HTML elements
     const slides = document.querySelectorAll('.slideshow img');
     const productsSection = document.querySelectorAll('.products');
+
+    let cart = [];
+    let currentSlide = 0;
     let productId = 0;
+    const productsData = await getProductsData();
 
     function addToCart(name, price) {
         cart.push({ name, price });
@@ -16,12 +19,40 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     
     async function getProductsData() {
-        const result = await $.get('/api/products');
-        return result;
+        const products = await $.get('/api/products');
+        return products;
     }
 
-    async function createProducts() {
-        const products = await getProductsData();
+    function filterProductsData() {
+        const filter = document.getElementById('filter').value;
+
+        // Filter products based on filter value
+        const filteredProducts = productsData.filter((product) => {
+            if(filter == 'color-mixed') {
+                return product.color == 'mixed';
+            } else if(filter == 'color-red') {
+                return product.color == 'red';
+            } else if(filter == 'color-yellow') {
+                return product.color == 'yellow';
+            } else if(filter == 'occasion-any') {
+                return product.occasion == 'any';
+            } else if(filter == 'type-arrangement') {
+                return product.type == 'arrangement';
+            } else if(filter == 'type-bouquet') {
+                return product.type == 'bouquet';
+            } else if(filter == 'type-garden') {
+                return product.type == 'garden';
+            } else {
+                return true;        
+            }
+        });
+
+        return filteredProducts;
+    }
+
+    function createProducts() {
+        productsSection[0].innerHTML = '';   
+        const products = filterProductsData();
         
         // Add products to products section
         products.forEach((product) => {
@@ -31,10 +62,16 @@ document.addEventListener('DOMContentLoaded', () => {
             productElement.innerHTML = `
             <img src=${product.imgSrc} alt="${product.name}" />
             <h3>${product.name}</h3>
-            <p>₱${product.price}</p>`;
+            <p>₱${product.price}</p>
+            <p>Stock: ${product.stock}</p>`;
             
             const button = document.createElement('button');
-            button.textContent = 'Add to Cart';
+            if(product.stock == 0) {
+                button.disabled = true;
+                button.textContent = 'Out of Stock';
+            } else {
+                button.textContent = 'Add to Cart';
+            }
             button.onclick = () => addToCart(product.name, product.price);
             productElement.appendChild(button);
 
@@ -59,4 +96,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setInterval(nextSlide, 5000); // Change slide every 5 seconds
     createProducts();
+    filter.onchange = createProducts;
 });
