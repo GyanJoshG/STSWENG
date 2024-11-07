423ca7a08202789c96f9a6c1df654f4752af5e0f
brent-soan <147591053+brent-soan@users.noreply.github.com>
2024-11-07 14:22:02 +0800

    Updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -10,11 +10,44 @@ document.addEventListener('DOMContentLoaded', function() {
         const email = formData.get('email');
         const password = formData.get('password');
 
-        if(email.length > 254) {
-            utils.inform(true, 'Error signing up: Email should not exceed 254 characters');
-            return;
-        }
-
         // TODO: Finish login script
+        try {
+            $.get('/api/users/exists', { username, email }, async (res) => {
+                if(res.data.usernameExists) {
+                    utils.inform(true, 'Error signing up: Username already exists. Use a different username.');
+                    return;
+                }
+    
+                if(res.data.emailExists) {
+                    utils.inform(true, 'Error signing up: Email already exists. Use a different email.');
+                    return;
+                }
+    
+                // Send POST request to /api/users/create
+                fetch(form.action, // URI
+                    {
+                        method: form.method, // POST request
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({ username, email, password, confirmPassword }),
+                    }
+                ).then(async (res) => {
+                    const result = await res.json();
+
+                    if(res.ok) {
+                        utils.inform(false, result.message);
+                        event.target.reset();
+                    } else {
+                        utils.inform(true, `Error signing up: ${result.error}`);
+                    }
+                })
+                .catch((err) => { // Catch POST request errors
+                    utils.inform(true, `Submission failed: ${err}`);
+                });
+            }).fail((jqXHR, textStatus, err) => { // Catch GET request errors
+                utils.inform(true, `Request failed: ${err}`);
+            });
+        } catch (err) {
+            utils.inform(true, `Unexpected error occurred: ${err}`);
+        }
     });
 });
