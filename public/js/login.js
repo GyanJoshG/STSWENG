f3fd295a27e140dfe2ba59088f43e235b5f21374
Carlos Rivera <josecirivera1906@gmail.com>
2024-11-12 12:58:06 +0800

    Updated css, html, and images

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('login-form');
+    const showPassword = document.querySelector('.show-password i');
 
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -10,5 +11,23 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Login attempt:', { username, password });
 
         loginForm.reset();
+    });
+
+    showPassword.addEventListener("click", function() {
+        if(passwordField.type === "password") {
+            passwordField.type = "text";
+            showPassword.classList.remove("fa-eye");
+            showPassword.classList.add("fa-eye-slash");
+        } else {
+            passwordField.type = "password";
+            showPassword.classList.remove("fa-eye-slash");
+            showPassword.classList.add("fa-eye");
+        }
+        // if clicked, change the color of the icon to #007bff, and if clicked again, change it back to #e38eda
+        if(showPassword.style.color === "#e38eda") {
+            showPassword.style.color = "#504949";
+        } else {
+            showPassword.style.color = "#e38eda";
+        }
     });
 });
